2695c0e6e054bfa2384a0dd99af712357d1dcc03
Andreas Groll <andreas.groll@exxeta.com>
2020-03-27 15:15:19 +0100

    send confirmation mail with attached pdf

--- execute_deploy.sh
+++ execute_deploy.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+: "$S3_BUCKET_NAME"
+: "$STACK_NAME"
+: "$MAIL_SENDER"
+: "$MAIL_RECIPIENT"
+
 cd lambda/tinchen
 zip ../tinchen.zip ./*.py
 cd $OLDPWD
 
 cd cfn
 aws cloudformation package --template aws-resources.yaml --s3-bucket ${S3_BUCKET_NAME} --output-template template-export.yaml
-aws cloudformation deploy  --template-file=template-export.yaml --stack-name="${STACK_NAME}" --capabilities=CAPABILITY_NAMED_IAM
+aws cloudformation deploy  --template-file=template-export.yaml --stack-name="${STACK_NAME}" --capabilities=CAPABILITY_NAMED_IAM --parameter-overrides MailSender=${MAIL_SENDER} MailRecipient=${MAIL_RECIPIENT} 
--- lambda/tinchen/app.py
+++ lambda/tinchen/app.py
@@ -10,19 +10,26 @@ import string
 import boto3
 import urllib
 
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
+from email.mime.application import MIMEApplication
+
 print('Loading function')
 
 s3 = boto3.client('s3')
 s3r = boto3.resource('s3')
 xmlDir = "/tmp/output/"
 
+bucket = os.getenv('SesBucketName')
+mailSender = os.getenv('MailSender')
+mailRecipient = os.getenv('MailRecipient')
 
 
 
 def lambda_handler(event, context):
     print('debug event: {}'.format(event))
     #bucket = event['Records'][0]['s3']['bucket']['name']
-    bucket = USER = os.getenv('SesBucketName')
+    
     key = urllib.parse.unquote_plus(event['Records'][0]['ses']['mail']['messageId'], 'utf8')
 
     try:
@@ -61,6 +68,15 @@ def processMail(msg):
                 link = getLink(part.get_payload(decode=True))
                 download(link, '/tmp/speiseplan.pdf')
                 print('downloaded!')
+                sender_ = mailSender
+                recipients_ = [mailRecipient]
+                title_ = 'Email title here'
+                text_ = 'Speiseplan'
+                body_ = """<html><head></head><body><h1>Speiseplan</h1><br>Some text."""
+                attachments_ = ['/tmp/speiseplan.pdf']
+
+                response_ = send_mail(sender_, recipients_, title_, text_, body_, attachments_)
+                print(response_)
                 #print(msgpart.get_payload(decode=True))
                 #body = msgpart.get_payload(decode=True)  # decode
                 #print(body)
@@ -109,6 +125,60 @@ def delete_file(key, bucket):
     s3.delete_object(Bucket=bucket, Key=key)
     print("%s deleted fom %s ") % (key, bucket)
 
+def create_multipart_message(
+        sender: str, recipients: list, title: str, text: str=None, html: str=None, attachments: list=None)\
+        -> MIMEMultipart:
+    """
+    Creates a MIME multipart message object.
+    Uses only the Python `email` standard library.
+    Emails, both sender and recipients, can be just the email string or have the format 'The Name <the_email@host.com>'.
+
+    :param sender: The sender.
+    :param recipients: List of recipients. Needs to be a list, even if only one recipient.
+    :param title: The title of the email.
+    :param text: The text version of the email body (optional).
+    :param html: The html version of the email body (optional).
+    :param attachments: List of files to attach in the email.
+    :return: A `MIMEMultipart` to be used to send the email.
+    """
+    multipart_content_subtype = 'alternative' if text and html else 'mixed'
+    msg = MIMEMultipart(multipart_content_subtype)
+    msg['Subject'] = title
+    msg['From'] = sender
+    msg['To'] = ', '.join(recipients)
+
+    # Record the MIME types of both parts - text/plain and text/html.
+    # According to RFC 2046, the last part of a multipart message, in this case the HTML message, is best and preferred.
+    if text:
+        part = MIMEText(text, 'plain')
+        msg.attach(part)
+    if html:
+        part = MIMEText(html, 'html')
+        msg.attach(part)
+
+    # Add attachments
+    for attachment in attachments or []:
+        with open(attachment, 'rb') as f:
+            part = MIMEApplication(f.read())
+            part.add_header('Content-Disposition', 'attachment', filename=os.path.basename(attachment))
+            msg.attach(part)
+
+    return msg
+
+
+def send_mail(
+        sender: str, recipients: list, title: str, text: str=None, html: str=None, attachments: list=None) -> dict:
+    """
+    Send email to recipients. Sends one mail to all recipients.
+    The sender needs to be a verified email in SES.
+    """
+    msg = create_multipart_message(sender, recipients, title, text, html, attachments)
+    ses_client = boto3.client('ses')  # Use your settings here
+    return ses_client.send_raw_email(
+        Source=sender,
+        Destinations=recipients,
+        RawMessage={'Data': msg.as_string()}
+    )
 
 if __name__ == '__main__':
     f = open('lambda/tinchen/email.sample', 'r')
